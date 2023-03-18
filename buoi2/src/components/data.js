00ff83696cc9ee5b3ce4a12f1e584ef979d0abd1
Nguyen-Duy-Son <nguyenduyson@28072003@email.com>
2023-03-18 14:40:09 +0700

    init prj

---
+++ buoi2/src/components/data.js
@@ -0,0 +1,49 @@
+
+import img1 from './Image1.png';
+import img2 from './Image2.png';
+import img3 from './Image3.png';
+import img4 from './Image4.png';
+import reg1 from './Rectangle1.png';
+import reg2 from './Rectangle2.png';
+import reg3 from './Rectangle3.png';
+import reg4 from './Rectangle4.png';
+import ellipse1 from './Ellipse1.png';
+import vector1 from './Vector1.png';
+export const data = [
+    {
+        id: '1',
+        title: "Search Data",
+        src: img1,
+        ellipse:ellipse1,
+        rectangle: reg1,
+        vector: vector1,
+        content: 'Don’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time.'
+    },
+    {
+        id: 2,
+        title: "24 Hours Access",
+        src: img2,
+        ellipse: ellipse1,
+        rectangle: reg2,
+        vector: vector1,
+        content: "Access is given 24 hours a full morning to night and meet again in the morning, giving you comfort when you need data when urgent."
+    },
+    {
+        id: 3,
+        title: "Print Out",
+        src: img3,
+        ellipse: ellipse1,
+        rectangle: reg3,
+        vector: vector1,
+        content: "Access is given 24 hours a full morning to night and meet again in the morning, giving you comfort when you need data when urgent."
+    },
+    {
+        id: 4,
+        title: "Security Code",
+        src: img4,
+        ellipse: ellipse1,
+        rectangle: reg4,
+        vector: vector1,
+        content: "Access is given 24 hours a full morning to night and meet again in the morning, giving you comfort when you need data when urgent."
+    }
+]
